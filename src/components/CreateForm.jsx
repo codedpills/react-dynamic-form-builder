8666dc0504a81e7b4917e0e6ebf795c289f91789
zak <codedpills@gmail.com>
2020-12-13 16:03:11 +0000

    add dynamic inputs to state and pass them as props

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import DynamicForm from "./DynamicForm";
 import InputButtons from "./InputButtons";
 
 class CreateForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formInputs: [
+        {
+          name: "singleline-123lkfs",
+          type: "singleLine",
+          label: "Single Line",
+          placeholder: "Enter text",
+          predefinedValue: "",
+        },
+        {
+          name: "multiline-123lkfs",
+          type: "multiLine",
+          label: "Multiline",
+          placeholder: "Enter long text",
+          predefinedValue: "",
+        },
+        {
+          name: "number-123lkfs",
+          type: "number",
+          label: "Number",
+          placeholder: "Enter number",
+          predefinedValue: "",
+        },
+        {
+          name: "email-123lkfs",
+          type: "email",
+          label: "Email",
+          placeholder: "Enter email",
+          predefinedValue: "",
+        },
+        {
+          name: "date-123lkfs",
+          type: "date",
+          label: "Date",
+          placeholder: "Enter date",
+          predefinedValue: "",
+        },
+      ],
+    };
+  }
   render() {
     return (
       <Container>
@@ -12,9 +55,11 @@ class CreateForm extends Component {
             <hr />
             <InputButtons />
           </Col>
+          <Col xs={1}></Col>
           <Col>
             <h5>Form</h5>
             <hr />
+            <DynamicForm formInputs={this.state.formInputs} />
           </Col>
         </Row>
       </Container>
