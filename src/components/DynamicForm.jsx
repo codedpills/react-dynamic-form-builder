7a95f4464dced1f4cf1d2c15d9b680703c2d4c63
tony <toneyobaze@gmail.com>
2020-12-15 11:46:52 +0000

    add delete-button

--- src/components/DynamicForm.jsx
+++ src/components/DynamicForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, Col } from "react-bootstrap";
+import InputOptionButtons from "./InputOptionButtons";
 
 const DynamicForm = ({ formInputs }) => {
   const formFields = formInputs.map((input, i) => {
@@ -7,14 +8,30 @@ const DynamicForm = ({ formInputs }) => {
       case "singleLine":
         return (
           <Form.Group controlId={input.name} key={input.name}>
-            <Form.Label>{input.label}</Form.Label>
+            <Form.Row>
+              <Col>
+                <Form.Label>{input.label}</Form.Label>
+              </Col>
+
+              <Col className="text-right">
+                <InputOptionButtons />
+              </Col>
+            </Form.Row>
             <Form.Control type="text" placeholder={input.placeholder} />
           </Form.Group>
         );
       case "multiLine":
         return (
           <Form.Group controlId={input.name} key={input.name}>
-            <Form.Label>{input.label}</Form.Label>
+            <Form.Row>
+              <Col>
+                <Form.Label>{input.label}</Form.Label>
+              </Col>
+
+              <Col className="text-right">
+                <InputOptionButtons />
+              </Col>
+            </Form.Row>
             <Form.Control
               as="textarea"
               rows={3}
@@ -25,28 +42,60 @@ const DynamicForm = ({ formInputs }) => {
       case "number":
         return (
           <Form.Group controlId={input.name} key={input.name}>
-            <Form.Label>{input.label}</Form.Label>
+            <Form.Row>
+              <Col>
+                <Form.Label>{input.label}</Form.Label>
+              </Col>
+
+              <Col className="text-right">
+                <InputOptionButtons />
+              </Col>
+            </Form.Row>
             <Form.Control type="number" placeholder={input.placeholder} />
           </Form.Group>
         );
       case "email":
         return (
           <Form.Group controlId={input.name} key={input.name}>
-            <Form.Label>{input.label}</Form.Label>
+            <Form.Row>
+              <Col>
+                <Form.Label>{input.label}</Form.Label>
+              </Col>
+
+              <Col className="text-right">
+                <InputOptionButtons />
+              </Col>
+            </Form.Row>
             <Form.Control type="email" placeholder={input.placeholder} />
           </Form.Group>
         );
       case "date":
         return (
           <Form.Group controlId={input.name} key={input.name}>
-            <Form.Label>{input.label}</Form.Label>
+            <Form.Row>
+              <Col>
+                <Form.Label>{input.label}</Form.Label>
+              </Col>
+
+              <Col className="text-right">
+                <InputOptionButtons />
+              </Col>
+            </Form.Row>
             <Form.Control type="date" placeholder={input.placeholder} />
           </Form.Group>
         );
       case "time":
         return (
           <Form.Group controlId={input.name} key={input.name}>
-            <Form.Label>{input.label}</Form.Label>
+            <Form.Row>
+              <Col>
+                <Form.Label>{input.label}</Form.Label>
+              </Col>
+
+              <Col className="text-right">
+                <InputOptionButtons />
+              </Col>
+            </Form.Row>
             <Form.Control type="time" placeholder={input.placeholder} />
           </Form.Group>
         );
